d397dd60322389446c7b2780a028667448c5d1b4
Trevor Dorsey <tsdorsey@gmail.com>
2019-12-18 17:44:13 -0800

    Bug Fix: javascript running in timezones other than UTC failed to product the expected result

--- tests/buildWithOffset.test.ts
+++ tests/buildWithOffset.test.ts
@@ -1,8 +1,8 @@
 import { buildISOStringWithOffset } from '../src/calculator';
 
 test('buildISOStringWithOffset', () => {
-  expect(buildISOStringWithOffset(new Date(2019, 11, 17, 1, 2, 3, 4), -90)).toBe('2019-12-17T02:32:03.004+01:30');
-  expect(buildISOStringWithOffset(new Date(2019, 11, 17, 1, 2, 3, 4), 480)).toBe('2019-12-16T17:02:03.004-08:00');
-  expect(buildISOStringWithOffset(new Date(2019, 11, 17, 1, 2, 3, 4), 420)).toBe('2019-12-16T18:02:03.004-07:00');
-  expect(buildISOStringWithOffset(new Date(2019, 11, 17, 1, 2, 3, 4), 0)).toBe('2019-12-17T01:02:03.004+00:00');
+  expect(buildISOStringWithOffset(new Date('2020-01-01T12:13:14.015Z'), -90)).toBe('2020-01-01T13:43:14.015+01:30');
+  expect(buildISOStringWithOffset(new Date('2020-01-01T12:13:14.015Z'), 480)).toBe('2020-01-01T04:13:14.015-08:00');
+  expect(buildISOStringWithOffset(new Date('2020-01-01T12:13:14.015Z'), 420)).toBe('2020-01-01T05:13:14.015-07:00');
+  expect(buildISOStringWithOffset(new Date('2020-01-01T12:13:14.015Z'), 0)).toBe('2020-01-01T12:13:14.015+00:00');
 });
